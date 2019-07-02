0e61a0a6ae3e79bc345232603bc3bc60399d0f59
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-07-02 00:25:23 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -34,8 +34,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array.slice(1);
-  return array;
+  return array.slice(1);
+ 
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
