6fa4e2e8d7a80a5698dfb5802fa6d052a397c244
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-07-02 00:21:42 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -43,7 +43,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
   return array;
 }
 
-function destructivelyRemoveElementFromEndOfArray(array){
+function removeElementFromEndOfArray(array){
   array.slice(0, array.length -1);
   return array;
 }
